1ebcdb07614b7b0ed491dcc5bbffe2ffddd5125d
Chanbong Park <siprikorea@gmail.com>
2025-03-21 00:20:15 +0900

    Initial Commit

---
+++ src/main/java/com/siprikorea/webserver/ClientController.java
@@ -0,0 +1,16 @@
+package com.siprikorea.webserver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/jsp")
+public class ClientController {
+    @RequestMapping("/{path}/**")
+    public String process(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        path = path.replaceFirst("/jsp", "");
+        return path;
+    }
+}
